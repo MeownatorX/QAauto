eb98e66a3f9f8d77a45304b2353904e3abf042ba
MeownatorX <genofrivia@gmail.com>
2026-05-09 01:55:58 +0200

    Add files via upload
    Java operators and loops

--- Test.java
+++ Test.java
@@ -1,277 +1,401 @@
-package practice;
+package practice3;
+import java.util.*;
 
 public class Test {
-    public static void main(String[] args){
-        Car car = new Car("Kia",2018);
-        car.setYear(2022);
-        car.showInfo();
-
-        Rectangle rectangle = new Rectangle(6,9);
-        rectangle.setWidth(12);
-        System.out.println(rectangle.calculateArea());
-
-        Book book = new Book("GOT","Martin");
-        book.setAuthor("Petya");
-        book.showInfo();
-
-        BankAccount ba = new BankAccount(1000, "Viktor");
-        ba.deposit(300.5);
-        ba.printBalance();
-
-        Point point = new Point(50,22);
-        point.setX(11);
-        point.print();
-
-        StudentGroup sg = new StudentGroup("Vasilki",30);
-        sg.setCount(42);
-        sg.print();
-
-        Circle circle = new Circle(12);
-        circle.setRadius(11);
-        System.out.println(circle.calculateArea());
-        System.out.println(circle.calculateCircumference());
-
-        Teacher teacher = new Teacher("Nikolay","History");
-        teacher.setSubject("Math");
-        teacher.print();
-
-        Product product = new Product("Car",1800);
-        product.discount(2000);
-        product.discount(200);
-        product.printInfo();
-
-        Laptop laptop = new Laptop("Lenovo", 200);
-        laptop.setPrice(300);
-        laptop.printInfo();
+    public static void main(String[] args) {
+        BreakProblems.method4();
     }
 }
-class Car{
-    Car(String brand, int year){
-        this.brand=brand;
-        this.year=year;
-    }
-    private String brand;
-    private int year;
-    String getBrand(){
-        return brand;
-    }
-    int getYear(){
-        return year;
-    }
-    void setBrand(String b){
-        brand=b;
-    }
-    void setYear(int y){
-        year=y;
-    }
-    void showInfo(){
-        System.out.println("Brand: " + brand + ", year: "+ year);
-    }
-}
-class Rectangle{
-    Rectangle(int length, int width){
-        this.length=length;
-        this.width=width;
-    }
-    private int length, width;
-    int getLength(){
-        return length;
-    }
-    int getWidth(){
-        return width;
-    }
-    void setWidth(int width){
-        this.width=width;
-    }
-    int calculateArea(){
-        return length*width;
-    }
-}
-class Book{
-    Book(String title, String author){
-        this.title=title;
-        this.author=author;
-    }
-    private String title, author;
-    String getTitle(){
-        return title;
-    }
-    String getAuthor(){
-        return author;
-    }
-    void setTitle(String title){
-        this.title=title;
-    }
-    void setAuthor(String author){
-        this.author=author;
-    }
-    void showInfo(){
-        System.out.println("Title: " + title + ", author: " + author);
-    }
-
-}
-class BankAccount{
-    BankAccount(double balance, String owner){
-        this.balance=balance;
-        this.owner=owner;
-    }
-    private double balance;
-    private String owner;
-
-    double getBalance(){
-        return balance;
-    }
-    String getOwner(){
-        return owner;
-    }
-    void setOwner(String owner){
-        this.owner=owner;
-    }
-    void deposit(double deposit){
-        balance+=deposit;
-    }
-    void withdraw(double withdraw){
-        if(withdraw<=balance){
-        balance-=withdraw;}
+class IfElseProblems{
+    static void method1(){
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        if(i>0){
+            System.out.println("The number is positive");
+        }
+        else if(i==0){
+            System.out.println("The number is zero");
+        }
         else {
-            System.out.println("You cannot withdraw this amount");
+            System.out.println("The number is negative");
         }
     }
-    void printBalance(){
-        System.out.println("Balance: " + balance);
+    static void method2(){
+        Scanner scan = new Scanner(System.in);
+        int i1 = scan.nextInt();
+        int i2 = scan.nextInt();
+        System.out.println(Math.max(i1,i2));
     }
-}
-class Point{
-    Point(int x, int y){
-        this.x=x;
-        this.y=y;
+    static void method3(){
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        switch (i){
+            case 5:
+                System.out.println("Great");
+            break;
+            case 4:
+                System.out.println("Good");
+                break;
+            case 3:
+                System.out.println("Ok");
+                break;
+            case 2:
+            case 1:
+                System.out.println("Very bad");
+                break;
+            default:
+                System.out.println("Illegal number");
+        }
     }
-    private int x,y;
-    int getX(){
-        return x;
-    }
-    int getY(){
-        return y;
-    }
-    void setX(int x){
-        this.x=x;
-    }
-    void print(){
-        System.out.println("X: " + x + " Y: " + y);
-    }
-}
-class StudentGroup{
-    StudentGroup(String name, int count){
-        this.count= count;
-        this.name=name;
-    }
-    private String name;
-    private int count;
-
-    String getName(){
-        return name;
-    }
-    int getCount(){
-        return count;
-    }
-    void setName(String name){
-        this.name=name;
-    }
-    void setCount(int count){
-        this.count=count;
-    }
-    void print(){
-        System.out.println("Group name: " + name + ", Student count: " + count);
-    }
-}
-class Circle{
-    Circle(int radius){
-        this.radius=radius;
-    }
-    private int radius;
-    int getRadius(){
-        return radius;
-    }
-    void setRadius(int radius){
-        this.radius=radius;
-    }
-    double calculateArea(){
-        return (double) 3.14*radius*radius;
-    }
-    double calculateCircumference(){
-        return (double) 2*3.14*radius;
-    }
-}
-class Teacher{
-    Teacher(String name, String subject){
-        this.name=name;
-        this.subject=subject;
-    }
-    private String name, subject;
-    void setName(String name){
-        this.name=name;
-    }
-    void setSubject(String subject){
-        this.subject=subject;
-    }
-    String getName(){
-        return name;
-    }
-    String getSubject(){
-        return subject;
-    }
-    void print(){
-        System.out.println("Name: " + name + " subject: " + subject);
-    }
-}
-class Product{
-    Product(String name, int price){
-        this.name=name;
-        this.price=price;
-    }
-    private String name;
-    private int price;
-    String getName(){
-        return name;
-    }
-    int getPrice(){
-        return price;
-    }
-    void setPrice(int price){
-            this.price = price;
-    }
-    void discount(int discount){
-        if(price-discount>=0) {
-            price-=discount;
+    static void method4(){
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        if(i>0){
+        if(i%2==0){
+            System.out.println("The number is even");
+        }
+        else {
+            System.out.println("The number is odd");
         }
         }
-    void printInfo(){
-        System.out.println("Product name: " + this.name + " price: " + price);
+        else
+            System.out.println("Weird number");
     }
-
+    static void method5(){
+        Scanner scan = new Scanner(System.in);
+        int age = scan.nextInt();
+        if(age<18){
+            System.out.println("The discount is 25%");
+        }
+        else if(age>=65){
+            System.out.println("The discount is 30%");
+        }
+        else{
+            System.out.println("No discount");
+        }
     }
-    class Laptop{
-    Laptop(String brand, int price){
-        this.brand=brand;
-        this.price=price;
+    static void method6(){
+        Scanner scan = new Scanner(System.in);
+        int grade = scan.nextInt();
+        if(grade>89){
+            System.out.println("Great");
+        }
+        else if(grade>74){
+            System.out.println("Good");
+        }
+        else if(grade>59){
+            System.out.println("Ok");
+        }
+        else if(grade>=0){
+            System.out.println("Very bad");
+        }
+        else
+            System.out.println("Negative number");
     }
-        private String brand;
-    private int price;
-    void setPrice(int price){
-        this.price=price;
+}
+class SwitchProblems{
+    static void method1(){
+        Scanner scan = new Scanner(System.in);
+        int day = scan.nextInt();
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Illegal number");
+        }
     }
-    void setBrand(String brand){
-        this.brand=brand;
+    static void method2(){
+        Scanner scan = new Scanner(System.in);
+        int day = scan.nextInt();
+        switch (day){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("300 rub");
+                break;
+            case 6:
+            case 7:
+                System.out.println("450 rub");
+                break;
+            default:
+                System.out.println("Illegal number");
+        }
     }
-    String getBrand(){
-        return brand;
+    static void method3(){
+        Scanner scan = new Scanner(System.in);
+        int grade = scan.nextInt();
+        if(grade>89){
+            System.out.println("A");
+        }
+        else if(grade>79){
+            System.out.println("B");
+        }
+        else if(grade>69){
+            System.out.println("C");
+        }
+        else if(grade>59){
+            System.out.println("D");
+        }
+        else if(grade>=0){
+            System.out.println("F");
+        }
+        else
+            System.out.println("Negative number");
     }
-    int getPrice(){
-        return price;
+    static void method4(){
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        switch (s){
+            case "stop":
+                System.out.println("Stopped");
+                break;
+            case "start":
+                System.out.println("Starting");
+                break;
+            case "restart":
+                System.out.println("Restarting");
+                break;
+            case "status":
+                System.out.println("All is great");
+                break;
+            default:
+                System.out.println("Brrr");
+        }
     }
-    void printInfo(){
-        System.out.println("Brand: " + brand + " price: " + price);
+    static void method5(){
+        Scanner scan = new Scanner(System.in);
+        int i1 = scan.nextInt();
+        String s = scan.next();
+        int i2 = scan.nextInt();
+        switch (s){
+            case "+":
+                System.out.println(i1+i2);
+                break;
+            case "-":
+                System.out.println(i1-i2);
+                break;
+            case "*":
+                System.out.println(i1*i2);
+                break;
+            case "/":
+                if(i1==0||i2==0){
+                    System.out.println("Cannot divide with zero");
+                    break;
+                }
+                else {
+                    System.out.println(i1/i2);
+                break;}
+            default:
+                System.out.println("Wrong operator");
+        }
     }
     }
-
+    class ForProblems{
+    static void method1(){
+        for(int i = 1; i<101; i++){
+            if(i%3==0){
+                System.out.println(i);
+            }
+        }
+    }
+    static void method2(){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int sum = n;
+        for(int i = 1; i<n; i++){
+            sum+=i;
+        }
+        System.out.println("Sum:" + sum);
+    }
+    static void method3(){
+        Scanner scan = new Scanner(System.in);
+        int digit = scan.nextInt();
+        for(int i = 1; i<11; i++){
+            System.out.println(digit + " * " + i + " = " + digit*i);
+        }
+    }
+    static void method4(){
+        Scanner scan = new Scanner(System.in);
+        int digit = scan.nextInt();
+        boolean isPrime=false;
+        for(int i = 2; i<digit; i++){
+            if(digit%i==0){
+                isPrime = false;
+                break;
+            }
+            else
+                isPrime = true;
+        }
+        System.out.println(isPrime);
+    }
+    static void method5(){
+        for(int i = 1; i<11; i++){
+            System.out.println(i);
+        }
+    }
+    }
+    class WhileProblems{
+    static void method1(){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int f = n;
+        int i = 1;
+        while(i<n){
+            f*=i;
+            i++;
+        }
+        System.out.println(f);
+    }
+    static void method2(){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        int i = 1;
+        System.out.println(i);
+        while(i<n){
+            if(i%2==0){
+                System.out.println(i);
+            }
+            i++;
+        }
+        System.out.println(n);
+    }
+    static void method3(){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int i = n;
+        while(i>0){
+            System.out.println(i);
+            i--;
+        }
+    }
+    }
+    class DoWhileProblems{
+    static void method1(){
+        Scanner scan = new Scanner(System.in);
+        int digit;
+        do{
+            digit = scan.nextInt();
+        }
+        while(digit<=0);
+    }
+    static void method2(){
+        Scanner scan = new Scanner(System.in);
+        String login = "admin";
+        String password = "123";
+        String login2, password2;
+        do{
+            login2 = scan.next();
+            password2 = scan.next();
+        }
+        while(!login.equals(login2)||!password.equals(password2));
+        System.out.println("Logged in successfully");
+    }
+    static void method3(){
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<11);
+    }
+    static void method4(){
+        Scanner scan = new Scanner(System.in);
+        String s;
+        do{
+            s = scan.next();
+            System.out.println("Going");
+        }
+        while(!s.equals("exit"));
+    }
+    static void method5(){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int n2 = n;
+        int result = 0;
+        if(n>0){
+            do{
+                if(n%10!=0){
+                    result++;
+                    n2 = n2/10;
+                }
+            }
+            while (n2>0);
+        }
+        System.out.println(result);
+    }
+    }
+    class BreakProblems{
+    static void method1(){
+        Scanner scan = new Scanner(System.in);
+        int input = 0;
+        int sum = 0;
+        while(true){
+            input = scan.nextInt();
+            if(input<0){
+                break;
+            }
+            sum+=input;
+            System.out.println(sum);
+        }
+    }
+    static void method2(){
+        for(int i = 1; i<21; i++){
+            if(i%3==0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+    static void method3(){
+        Scanner scan = new Scanner(System.in);
+        int [] array = new int [4];
+        int i = 0;
+        int f;
+        while(i< array.length){
+            f = scan.nextInt();
+            if(f>=0){
+            array[i] = f;
+            }
+            i++;
+        }
+        for (int a: array){
+            if(a!=0) {
+                System.out.println(a);
+            }
+        }
+    }
+    static void method4(){
+        Scanner scan = new Scanner(System.in);
+        String s;
+        while(true){
+            s = scan.next();
+            if(s.equals("stop")){
+                System.out.println("Stopped");
+                break;
+            }
+        }
+    }
+    }
 
